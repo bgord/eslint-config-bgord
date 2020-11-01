58a755adec162d1864e641c11fb46c3ee974d203
Bartosz Gordon <bartosz.gordon@minebest.com>
2020-11-01 13:14:31 +0100

    Use a base.sh script to predefine log functions

---
+++ scripts/base.sh
@@ -0,0 +1,12 @@
+function info {
+  echo "[info]      $1"
+}
+
+function success {
+  echo "[success]   $1"
+}
+
+function error {
+  echo "[error]     $1"
+}
+
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
+# Preload info, success, and error function from the base.sh file.
+source scripts/base.sh
+
 # The script will quit/abort after the first failed command.
 set -e
 
 VERSION_CHANGE=$1
-echo "[info]      Requested version change: '$VERSION_CHANGE'."
+info "Requested version change: '$VERSION_CHANGE'."
 
 
 # Check if $VERSION_CHANGE adjusts to the semantic versioning scheme: https://semver.org/.
 if [[ $VERSION_CHANGE != "major" && $VERSION_CHANGE != "minor" && $VERSION_CHANGE != "patch" ]]; then
-  echo "[error]     Incorrect requested version, must be one of: major, minor, or patch."
-  echo "[error]     Quitting..."
+  error "Incorrect requested version, must be one of: major, minor, or patch."
+  error "Quitting..."
   exit 1
 else
-  echo "[success]   Correct version change, proceeding."
+  success "Correct version change, proceeding."
 fi
 
-echo "[info]      Applying npm package version change."
+info "Applying npm package version change."
 npm version $1
 
-echo "[info]      Pushing version change commit and git tag."
+info "Pushing version change commit and git tag."
 git push
 git push --tags
 
-echo "[info]      Logging into npm account."
+info "Logging into npm account."
 npm login
 
-echo "[info]      Dry-running the npm publish script."
+info "Dry-running the npm publish script."
 npm publish --dry-run
 
-echo "[info]      You have 10 seconds to cancel the publication of the packge..."
+info "You have 10 seconds to cancel the publication of the packge..."
 sleep 10s
 
-echo "[info]      You have decided to continue the publication, publishing..."
+info "You have decided to continue the publication, publishing..."
 npm publish
 
-echo "[success]   Package seems to be successfully published!"
+success "Package seems to be successfully published!"
